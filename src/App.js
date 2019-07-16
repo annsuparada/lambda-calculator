65e3fa10112fa5161aba46775325890c4aa9e838
Suparada Saitalae <ann.suparada@gmail.com>
2019-07-16 16:49:51 -0500

    map the numbers.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.scss";
 
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
-
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers"
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
@@ -18,7 +18,9 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
+        <Numbers />
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+
       </div>
     </div>
   );
